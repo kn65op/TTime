6561e0207d494774172880c15fcb17baf19344a0
Tomasz Drzewiecki <kn65op@gmail.com>
2013-08-26 19:59:53 +0200

    First interface version

--- TTime/Stoper.h
+++ TTime/Stoper.h
@@ -1,12 +1,77 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+namespace TTime
+{
+
+class StoperException
+{
+public:
+  /**
+   * Specify Exception description.
+   * @param desription
+   */
+  StoperException(std::string desription);
+  
+  /**
+   * Return std::string with exception description.
+   * @return Exception description.
+   */
+  operator std::string();
+  
+private:
+  std::string message;
+  
+};
+
+std::ofstream & operator<<(std::ostream & out, StoperException ex);
+
 class Stoper
 {
 public:
+  /**
+   * Default constructor. Creates simple stoper.
+   */
   Stoper(void);
+  
+  /**
+   * Destructor.
+   */
   ~Stoper(void);
 
-  void start();
+  /**
+   * Start stoper with specified name. 
+   * Name has to be unique. It is forbidden to start stoper, which has name that was already used.
+   * If name is in use StoperException will be thrown.
+   * @param name Stoper name.
+   */
+  static void start(std::string name);
+  
+  /**
+   * Start stoper.
+   * @param from_beginning Specify if stoper will start from 0 or from stopped time.
+   */
+  void start(bool from_beginning);
+  
+  /**
+   * Clear actual time. If stoper runs it will stop and set measured time to 0.
+   */
+  void clear();
+  
+  /**
+   * End stoper.
+   */
   void stop();
-  void getTime();
+  
+  /**
+   * Get measured time. If stoper runs it will return actual time.
+   * @return Measured time. 
+   */
+  long getTime();
+
+  
 };
 
+} //namespace TTime
